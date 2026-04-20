257293eb589cd0771d17976244f4238f41c99ed9
Erik Nordlund <erik.nordlund@digitalengine.se>
2026-04-20 10:44:22 +0200

    Initial commit

---
+++ src/main/java/com/penca/lapenca/LaPencaApplication.java
@@ -0,0 +1,13 @@
+package com.penca.lapenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaPencaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaPencaApplication.class, args);
+    }
+
+}
