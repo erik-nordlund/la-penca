af1fd34aac2f7ff536c2881adf75b89b1e9a1187
Erik Nordlund <erik.nordlund@digitalengine.se>
2026-05-09 20:00:02 +0200

    Testing to see if this maked the deployemtn faster(again)

--- src/main/java/com/penca/lapenca/security/JwtService.java
+++ src/main/java/com/penca/lapenca/security/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
@@ -12,13 +13,13 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private static final String SECRET =
-            "CHANGE_THIS_TO_A_LONG_SECRET_KEY_AT_LEAST_32_CHARACTERS";
+    @Value("${JWT_SECRET}")
+    private String secret;
 
     private static final long EXPIRATION_MS = 1000 * 60 * 60 * 24 * 7;
 
     private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
     public String generateToken(String username, String role) {
